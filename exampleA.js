07a4bd3aa4cb46a9a66c433630774a551cb78e9b
Adam Rackis <adamrackis@hotmail.com>
2018-01-10 16:08:00 -0600

    Further tweaks

--- exampleA.js
+++ exampleA.js
@@ -1,16 +1,20 @@
-import { action, observable, computed, autorun } from "mobx";
+import { action, observable, computed, autorun, runInAction } from "mobx";
 import ajaxUtil from "./ajaxUtil";
+
+let i = 0;
 
 export default class {
   @observable propA = 0;
   @observable propB = 0;
   @observable messages = [];
 
+  @observable runs = 0;
+
   clear = () => this.messages.clear();
 
   @computed
   get sum() {
-    return this.propA + this.propB + this.propC;
+    return this.propA + this.propB;
   }
 
   bumpA = () => this.propA++;
--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import { action, observable, computed } from "mobx";
 import ExampleA from "./exampleA";
 import ExampleB from "./exampleB";
 
-const store = new ExampleB();
+const store = new ExampleA();
 
 @observer
 export default class Main extends Component {
@@ -20,6 +20,7 @@ export default class Main extends Component {
         {store.bumpB ? <button onClick={store.bumpB}>Bump B</button> : null}
         {store.bumpAandB ? <button onClick={store.bumpAandB}>Bump A and B</button> : null}
         {store.clear ? <button onClick={store.clear}>Clear</button> : null}
+        {store.runs != null ? <span>{store.runs}</span> : null}
         <div>
           <ul>{store.messages.map(s => <li>{s}</li>)}</ul>
         </div>
