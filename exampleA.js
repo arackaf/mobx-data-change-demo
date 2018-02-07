cdc73cea309292b77aab71482c5c8cfb8e02768d
Adam Rackis <arackis@centralreach.com>
2018-02-07 15:06:56 -0500

    few more comments

--- exampleA.js
+++ exampleA.js
@@ -1,6 +1,7 @@
 import { action, observable, computed, autorun, runInAction } from "mobx";
 import ajaxUtil from "./ajaxUtil";
 
+//Hack workaround for runs #1
 let i = 0;
 
 export default class {
@@ -8,6 +9,7 @@ export default class {
   @observable propB = 0;
   @observable messages = [];
 
+  //will track total number of times our autorun has run, once it's added to the autorun
   @observable runs = 0;
 
   clear = () => this.messages.clear();
@@ -27,6 +29,7 @@ export default class {
   constructor() {
     //Track the READS
     autorun(() => {
+      //Output a message so we know every time this runs
       this.messages.push("autorun started");
 
       let a = this.propA;
