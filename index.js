602656393ea1d5a48eb4dd0e1f784d482a3e7ad0
Adam Rackis <adamrackis@hotmail.com>
2018-03-10 13:46:25 -0600

    object demo

--- index.js
+++ index.js
@@ -8,36 +8,31 @@ import ExampleB from "./exampleB";
 
 //const store = new ExampleA();
 
-class WithMap {
-  @observable map = new Map([]);
-  i = 1;
-  add = () => (this.map.set(this.i + "", this.i), this.i++);
+let alphabet = [..."abcdefghijklmnopqrstuvwxyz"];
 
-  @observable foo = {};
-
-  constructor() {
-    observe(this, "foo", props => {
-      debugger;
-    });
-
-    setTimeout(() => (this.foo.blah = 12), 1000);
-    setTimeout(() => alert(this.foo.blah), 1010);
-  }
+class WithObject {
+  @observable.ref obj = {};
+  i = 0;
+  add = () => {
+    let newLetter = alphabet[this.i++];
+    this.obj = { ...this.obj, [newLetter]: `Letter: ${newLetter}` };
+    debugger;
+  };
 }
 
-let store = new WithMap();
+let store = new WithObject();
 
 @observer
 export default class Main extends Component {
   render() {
     return (
       <div>
-        {[...store.map].map(([k, v]) => (
-          <span>
-            {k} {v}
-          </span>
-        ))}
         <button onClick={store.add}>Add</button>
+        {Object.entries(store.obj).map(([k, v]) => (
+          <div>
+            {k} {v}
+          </div>
+        ))}
       </div>
     );
   }
